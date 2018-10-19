957680375e7d0314519f52902807b2133977f38d
haryo adi nugroho <han@voxteneo.asia>
2018-10-19 16:58:26 +0700

    update example grid

--- src/stubs/examples/admin.blade.php
+++ src/stubs/examples/admin.blade.php
@@ -17,7 +17,7 @@
 
         {{--title--}}
         <div class="row">
-            <h1 class="h3 col-sm-6">User Management</h1>
+            <h1 class="h3 col-sm-6">Page Title</h1>
 
             <div class="col-sm-6">
                 <a href="#" class="btn btn-primary float-sm-right"><i class="fas fa-plus"></i> New</a>
@@ -62,11 +62,11 @@
 
                 {{--select2 zero config--}}
                 <div class="form-group form-row">
-                    <div class="col-sm-2 col-form-label">
+                    <div class="col-md-2 col-form-label">
                         <label for="select2">select2</label>
                     </div>
 
-                    <div class="col-sm-6">
+                    <div class="col-md-6">
                         <select id="select2" class="form-control" data-toggle="select2">
                             <option>options</option>
                             <option>options</option>
@@ -74,19 +74,19 @@
                         </select>
                     </div>
 
-                    <div class="col-sm-4 col-form-label">
+                    <div class="col-md-4 col-form-label">
                         <p class="text-danger">optional error message</p>
                     </div>
                 </div>
 
                 {{--datetimepicker js--}}
                 <div class="form-group form-row">
-                    <div class="col-sm-2 col-form-label">
+                    <div class="col-md-2 col-form-label">
                         <label for="datetimepicker">datetimepicker</label>
                         <p class="text-muted">using font awesome 5 icons</p>
                     </div>
 
-                    <div class="col-sm-6">
+                    <div class="col-md-6">
                         <div class=" input-group">
                             <span class="input-group-prepend">
                                 <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
@@ -98,26 +98,26 @@
 
                 {{--summernote--}}
                 <div class="form-group form-row">
-                    <div class="col-sm-2 col-form-label">
+                    <div class="col-md-2 col-form-label">
                         <label for="summernote">summernote</label>
                     </div>
 
-                    <div class="col-sm-6">
+                    <div class="col-md-6">
                         <textarea id="summernote" class="form-control" data-toggle="summernote"></textarea>
                     </div>
 
-                    <div class="col-sm-4">
+                    <div class="col-md-4">
                     </div>
                 </div>
 
                 {{--image upload preview--}}
                 <div class="form-group form-row">
-                    <div class="col-sm-2 col-form-label">
+                    <div class="col-md-2 col-form-label">
                         <label for="dropzone">dropzone</label>
                         <p class="text-muted">file uploader</p>
                     </div>
 
-                    <div class="col-sm-6">
+                    <div class="col-md-6">
                         <div  id="dropzone" class="dropzone dz-clickable form-control" data-toggle="dropzone" data-url="/" >
                             <div class="dz-message h5">
                                 Drop files here or click to upload.
@@ -125,7 +125,39 @@
                         </div>
                     </div>
 
-                    <div class="col-sm-4">
+                    <div class="col-md-4">
+                    </div>
+                </div>
+
+                {{--markdown parser--}}
+                <div class="form-group form-row">
+                    <div class="col-md-2 col-form-label">
+                        <label for="markdown">showdown</label>
+                        <p class="text-muted"># hello markdown!</p>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div data-toggle="markdown"># hello markdown!</div>
+                    </div>
+
+                    <div class="col-md-4">
+                    </div>
+                </div>
+
+                {{--markdown preview--}}
+                <div class="form-group form-row">
+                    <div class="col-md-2 col-form-label">
+                        <label for="markdown">showdown + xss filter</label>
+                        <p class="text-muted">markdown preview</p>
+                    </div>
+
+                    <div class="col-md-6">
+                        <textarea id="markdown" class="form-control" rows="3"></textarea>
+                        <button type="button" class="btn btn-secondary" data-toggle="markdown-preview" data-source="#markdown" data-target="#markdown-preview">preview</button>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div id="markdown-preview"></div>
                     </div>
                 </div>
 
