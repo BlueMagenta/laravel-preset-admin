2f2a0e4af817330c347a6049e8715846be8546d9
Haryo Adi <haryo.webapp@gmail.com>
2019-07-01 13:10:50 +0700

    use custom mix file instead of adding script to the existing one

--- src/admin.mix.js
+++ src/admin.mix.js
@@ -1,8 +1,11 @@
+const mix = require('laravel-mix');
+require('laravel-mix-merge-manifest');
 
 mix.js('resources/js/admin.js', 'public/js')
     .sass('resources/sass/admin.scss', 'public/css')
     .copy('node_modules/@fortawesome/fontawesome-free/svgs/solid/user.svg', 'public/img/user-default.svg')
+    .extract()
+    .mergeManifest()
     .autoload({
         jquery: ['$', 'jQuery', 'window.$', 'window.jQuery'],
-    })
-    .extract();
+    });
